b57f9a3df34e76f77be7c30d1d08f4fce2463da6
cheeeeeryou <cheryl9463@gmail.com>
2022-09-10 16:00:10 +0800

    Project done

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const exphbs = require('express-handlebars') // 套用handlebars
 const restaurantList = require('./restaurant.json') //套用餐廳名單
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
-const Restaurant = require("./models/Restaurant") //載入 Restaurant model
+const Restaurant = require('./models/Restaurant') //載入 Restaurant model
+// 引用 body-parser
+const bodyParser = require('body-parser')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 
 require('dotenv').config()
@@ -36,25 +40,70 @@ app.use(express.static('public'))//所有路由的請求都先走這
 // routes setting
 app.get('/', (req, res) => {
   Restaurant.find()
-    .lean()          // 把 Mongoose 的 Model 物件，轉換成乾淨單純的 JS 資料陣列
+    .lean()
     .then(restaurantsData => res.render("index", { restaurantsData }))
-    .catch(error => console.error(error)) //如果發生意外，執行錯誤處理
+    .catch(error => console.error(error))
 })
+
 
 //setting search function
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.toLowerCase()
-  const restaurants = restaurantList.results.filter(restaurant => {
-    return restaurant.name.toLowerCase().includes(keyword) || restaurant.category.includes(keyword)
-  }) //餐廳名稱 or(||) 種類
-  res.render('index', { restaurants, keyword })
+  Restaurant.find({})
+    .lean()
+    .then(restaurantsData => {
+      const filterRestaurantsData = restaurantsData.filter(
+        data =>
+          data.name.toLowerCase().includes(keyword) ||
+          data.category.includes(keyword)
+      )
+      res.render("index", { restaurantsData: filterRestaurantsData, keyword })
+    })
+})
+
+app.get('/restaurant/new', (req, res) => {
+  return res.render("new")
+})
+
+app.post('/restaurant', (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
+
+app.get('/restaurant/:restaurant_id/edit', (req, res) => {
+  const id = req.params.restaurant_id
+  Restaurant.findById(id)
+    .lean()
+    .then((restaurantsData) => res.render('edit', { restaurantsData }))
+    .catch(error => console.log(error))
+})
+
+app.put("/restaurant/:restaurant_id", (req, res) => {
+  const id = req.params.restaurant_id
+  Restaurant.findByIdAndUpdate(id, req.body)
+    .then(() => res.redirect(`/restaurant/${id}`))
+    .catch(err => console.log(err))
 })
 
 //render information page(show.handlebars)
 app.get('/restaurant/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
-}) //參考教案movie list的寫法，用3等號 + toString() 
+  const id = req.params.restaurant_id
+  Restaurant.findById(id)
+    .lean()
+    .then(restaurantsData =>
+      res.render("show", { restaurantsData }))
+    .catch(error => console.log(error))
+
+})
+
+app.delete('/restaurant/:restaurant_id', (req, res) => {
+  const id = req.params.restaurant_id
+  Restaurant.findByIdAndDelete(id)
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 // start and listen on the Express server
 app.listen(port, () => {
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
-
   name: { type: String, required: true },
   name_en: { type: String, required: true },
   category: { type: String, required: true },
