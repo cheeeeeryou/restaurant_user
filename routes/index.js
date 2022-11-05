1020434a9782669e8c793a1f8b9d43556eb8757c
cheeeeeryou <cheryl9463@gmail.com>
2022-11-06 00:06:15 +0800

    feat: add user modules

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,11 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const Restaurant = require('./modules/restaurants')
+const users = require('./modules/users') 
 
 // 準備引入路由模組
 router.use('/', home)
 router.use('/restaurant', Restaurant)
+router.use('/users', users) 
 
 module.exports = router // 匯出路由模組，要記得寫！不然會出錯
