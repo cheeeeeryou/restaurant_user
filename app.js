3ea4c2328138214ad0e63c8538d32693c89ec70d
cheeeeeryou <cheryl9463@gmail.com>
2022-11-06 08:35:46 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose') //套用mongoose
 const methodOverride = require('method-override')
 const bodyParser = require('body-parser')// body-parser 進行前置處理以利用HTTP動詞
 const routes = require('./routes')// 將 request 導入路由器
+const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
 
 require('./config/mongoose')
 
@@ -20,6 +21,9 @@ app.use(session({
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
+
+usePassport(app)// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+
 app.use(express.static('public')) //載入CSS js等
 app.use(routes)//所有路由從routes走
 
